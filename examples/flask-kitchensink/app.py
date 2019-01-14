70165688ab501dea03c0c299f8962e0061bcce39
Pkhamwan <46610111+Pkhamwan@users.noreply.github.com>
2019-01-15 00:09:46 +0700

    Update app.py

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -42,8 +42,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+channel_secret = os.getenv('2910abfc8f92b765f601324d1b08da6a', None)
+channel_access_token = os.getenv('pdH87qlP3GJ+YjzwLm6tH6IV4E6xIJAH/BdVB7+Ob4SxDjisSl1kErwNeIWMFsBSLRDp/H5vaLGXboanLBBZW70FOGB/Lc86G3rDgB90tHsUl2rjUFtCR/lPJQoTWpLG/Y0hFalt/7cbURq4aq5SrAdB04t89/1O/w1cDnyilFU=', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
